ab8c56dc4fc164980a9224b0b9d4f84ae6d9a7f9
Medido1 <medi1222@gmail.com>
2025-04-08 19:19:31 +0100

    Create ticket component and wrap App in the provider from the outside so App has access to global context

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,33 @@ import logo from "./assets/images/logo-full.svg"
 import MobileForm from './components/MobileFrom';
 import patternTop from "./assets/images/pattern-squiggly-line-top.svg";
 import patternMobile from "./assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg";
-import { formContext, FormProvider } from './context/FormContext.jsx';
+import { formContext } from './context/FormContext.jsx';
+import { useContext} from 'react';
+import Ticket from "./components/Ticket.jsx"
 
 function App() {
+  const {state, dispatch} = useContext(formContext);
+  const {isValid} = state;
+
   return (
-    <FormProvider>
-      <div className='min-h-screen w-screen bg-cover bg-center
-        flex justify-center text-center relative'
-        style={{backgroundImage: `url(${bgMobile})`}}>
-        <div>
-          <img className='pt-8 pb-8 justify-self-center' src={logo} alt="logo" />
-          <img src={patternTop} 
-            className="absolute top-4 right-0  w-24 object-cover z-0"/>
-          <img src={patternMobile} 
-            className='absolute bottom-0 h-32  object-cover z-0'
-          />
+    <div className='min-h-screen w-screen bg-cover bg-center
+      flex justify-center text-center relative'
+      style={{backgroundImage: `url(${bgMobile})`}}>
+      <div>
+        <img className='pt-8 pb-8 justify-self-center' src={logo} alt="logo" />
+        <img src={patternTop} 
+          className="absolute top-4 right-0  w-24 object-cover z-0"/>
+        <img src={patternMobile} 
+          className='absolute bottom-0 h-32  object-cover z-0'
+        />
+        {!isValid ? (
           <MobileForm />
-        </div>
+        ): (
+          <Ticket />
+        )}
+        
       </div>
-    </FormProvider>
+    </div>
   )
 }
 
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,9 @@
+function Ticket() {
+  return (
+    <p>
+      Test me test me say that you love me
+    </p>
+  )
+}
+
+export default Ticket;
